5e7610d059a5a94ad0dea16402772ded683c9a31
mariajoaovieira <mariaeirinha@gmail.com>
2020-07-15 00:16:53 +0100

    changed the css and added javascript data (city name, current temperature and condition)

--- script.js
+++ script.js
@@ -50,3 +50,19 @@ function searchCity(event) {
 
 let showCity = document.querySelector("#search-button");
 showCity.addEventListener("click", searchCity);
+
+// current temperature
+
+let apiKey = "c719d1f8f1ff494c66ad9db9e28a5999";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Albufeira&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherElement = document.querySelector("#condition");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherElement.innerHTML = response.data.weather[0].description;
+}
+
+axios.get(apiUrl).then(displayTemperature);
