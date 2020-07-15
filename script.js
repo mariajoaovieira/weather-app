7d4301446930d2c2149a097d024689dd9440eb31
mariajoaovieira <mariaeirinha@gmail.com>
2020-07-15 22:00:04 +0100

    added the current Data

--- script.js
+++ script.js
@@ -33,7 +33,13 @@ let day = days[now.getDay()];
 let month = months[now.getMonth()];
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${minutes}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let WeekDay = document.querySelector("#weekDay");
 weekDay.innerHTML = `${day}`;
@@ -51,18 +57,27 @@ function searchCity(event) {
 let showCity = document.querySelector("#search-button");
 showCity.addEventListener("click", searchCity);
 
-// current temperature
+// current data
 
 let apiKey = "c719d1f8f1ff494c66ad9db9e28a5999";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Albufeira&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let weatherElement = document.querySelector("#condition");
+  let maxTempElement = document.querySelector("#max-temp");
+  let minTempElement = document.querySelector("#min-temp");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherElement.innerHTML = response.data.weather[0].description;
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 axios.get(apiUrl).then(displayTemperature);
